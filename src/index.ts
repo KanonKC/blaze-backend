3a5142babb0fa18e39695419881ff8c103379e00
KanonKC <kanonkce@gmail.com>
2026-02-02 00:32:29 +0700

    fix: Expose service

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import server from "@/routes";
 
-server.listen({ port: 8080 }, (err, address) => {
+server.listen({ port: 8080, host: "0.0.0.0" }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
