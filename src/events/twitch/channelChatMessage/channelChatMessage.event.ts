b5c8eaff05deafd6d0959820ebbd90b0fa7409c7
KanonKC <kanonkce@gmail.com>
2026-02-14 14:41:22 +0700

    fix: Uncomment logger

--- src/events/twitch/channelChatMessage/channelChatMessage.event.ts
+++ src/events/twitch/channelChatMessage/channelChatMessage.event.ts
@@ -26,7 +26,7 @@ export default class TwitchChannelChatMessageEvent {
         const event = body.event as TwitchChannelChatMessageEventRequest
 
         if (body.subscription.status === "enabled") {
-            // this.logger.info({ message: "Handling chat message event", data: { broadcasterUserId: event.broadcaster_user_id, chatterUserId: event.chatter_user_id } });
+            this.logger.info({ message: "Handling chat message event", data: event });
             this.firstWordService.greetNewChatter(event)
             res.status(204).send()
             return
