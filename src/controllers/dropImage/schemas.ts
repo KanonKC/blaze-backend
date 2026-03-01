b1f3b74a25d77e4254c7abdd2cc3be55d46bdba4
kanon.che <kanon.che@ascendcorp.com>
2026-03-01 17:41:22 +0700

    fix: set max len message to 500

--- src/controllers/dropImage/schemas.ts
+++ src/controllers/dropImage/schemas.ts
@@ -5,9 +5,9 @@ export const createDropImageSchema = z.object({});
 export const updateDropImageSchema = z.object({
     twitch_reward_id: z.string().nullable().optional(),
     twitch_bot_id: z.string().nullable().optional(),
-    invalid_message: z.string().nullable().optional(),
-    not_image_message: z.string().nullable().optional(),
-    contain_mature_message: z.string().nullable().optional(),
+    invalid_message: z.string().max(500).nullable().optional(),
+    not_image_message: z.string().max(500).nullable().optional(),
+    contain_mature_message: z.string().max(500).nullable().optional(),
     enabled_moderation: z.boolean().optional(),
     enabled: z.boolean().optional(),
     display_duration: z.number().int().min(1).max(300).optional(),
